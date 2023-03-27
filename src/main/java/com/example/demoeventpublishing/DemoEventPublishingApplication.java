51bac8f4936977cf9963cedc063042c5c89f164c
Guillaume HUSTA <guillaume.husta@gmail.com>
2023-03-27 15:09:57 +0200

    fix: Field injection is not recommended

--- src/main/java/com/example/demoeventpublishing/DemoEventPublishingApplication.java
+++ src/main/java/com/example/demoeventpublishing/DemoEventPublishingApplication.java
@@ -3,7 +3,6 @@ package com.example.demoeventpublishing;
 import com.example.demoeventpublishing.model.Observation;
 import com.example.demoeventpublishing.service.MyService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,8 +19,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Slf4j
 public class DemoEventPublishingApplication implements CommandLineRunner {
 
-    @Autowired
-    private MyService myService;
+    private final MyService myService;
+
+    public DemoEventPublishingApplication(MyService myService) {
+        this.myService = myService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoEventPublishingApplication.class, args);
